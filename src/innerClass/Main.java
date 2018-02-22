cc5e5d0e6588e17b01ab9d0d0efa88d41cb63500
bwielk <bwielk@gmail.com>
2018-02-22 22:51:05 +0100

    Adding a button class as another example of the user of inner classes - in this case - interfaces

--- src/innerClass/Main.java
+++ src/innerClass/Main.java
@@ -1,8 +1,14 @@
 package innerClass;
 
+import java.util.Scanner;
+
 public class Main {
 	
+	private static Scanner scanner = new Scanner(System.in);
+	private static Button button = new Button("Print");
+	
 	public static void main(String[] args){
+		//Gearbox example
 		Gearbox mcLaren = new Gearbox(5);
 		mcLaren.operatingClutch(true);
 		mcLaren.changeGear(1);
@@ -14,6 +20,9 @@ public class Main {
 		mcLaren.changeGear(3);
 		mcLaren.operatingClutch(false);
 		System.out.println(mcLaren.wheelSpeed(1000));
+		System.out.println("/////////////////////////////////");
+		//Button example
+		
 	}
 
 }
